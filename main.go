951ac7e06d890a0e57b9e178bc1fc7a93e196c04
Arsh Malik <139640603+AyaHikari@users.noreply.github.com>
2024-10-05 19:25:46 +0500

    Update main.go

--- main.go
+++ main.go
@@ -2,35 +2,41 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/MinamFaisal/assignment01bca/assignment01bca" //package se functions use krengy
+	"github.com/AyaHikari/assignment01bca/assignment01bca" 
 )
 
 func main() {
-	// Genesis Block (1st block) iska previous hash kuch ni hga
-	genesisBlock := assignment01bca.NewBlock("genesis to Arsh", 1, "")
-	fmt.Println("\033[1;32m---------------------\033[0m")
-	fmt.Println("\033[1;32mGenesis Block created!\033[0m")
-	fmt.Println("\033[1;32m---------------------\033[0m")
+	// Create Genesis Block (first block) with no previous hash
+	genesisBlock := assignment01bca.NewBlock("Genesis to Luffy", 1, "")
+	fmt.Println("\n****************************************************************************************************\n")
+	fmt.Println("                                       Genesis Block Created\n")
+	fmt.Println("****************************************************************************************************\n")
 
-	// second block jo pcihly block k hash ko use kr k bnaengy
-	block1 := assignment01bca.NewBlock("Arsh to Mahareeb", 2, genesisBlock.Hash)
+	// Create second block using the hash of the Genesis block
+	block1 := assignment01bca.NewBlock("Luffy to Zoro", 2, genesisBlock.Hash)
 
-	// third block jo 1 wale block k hash ko use kr k aega
-	assignment01bca.NewBlock("Mahareeb to Maheen", 3, block1.Hash)
+	// Create third block using the hash of the second block
+	block2 := assignment01bca.NewBlock("Zoro to Sanji", 3, block1.Hash)
+	
+	// Create fourth block using the hash of the second block
+	assignment01bca.NewBlock("Sanji to Law", 4, block2.Hash)
 
-	// all blocks print on terminal
-	fmt.Println("\n\033[1;34mListing all blocks in the blockchain:\033[0m")
+	// Print the entire blockchain
+	fmt.Println("\n****************************************************************************************************\n")
+	fmt.Println("                            Displaying all blocks in the Blockchain\n")
+	fmt.Println("****************************************************************************************************\n")
 	assignment01bca.ListBlocks()
 
-	//change krrhe 1 block ko
-	fmt.Println("\n\033[1;33mChanging the transaction of Block 1...\033[0m")
-	assignment01bca.ChangeBlock(1, "Arsh to Mahareeb Updated")
+	// Modify the transaction in block 1
+	fmt.Println("\n****************************************************************************************************\n")
+	fmt.Println("                                  Modifying Block 1 Transaction...")
+	fmt.Println("\n****************************************************************************************************\n")
+	assignment01bca.ChangeBlock(1, "Luffy to Zoro Updated")
 	assignment01bca.ListBlocks()
 
-	// Verify the blockchain
-	fmt.Println("\033[1;35m--------------------------------------------\033[0m")
-	fmt.Println("\n\033[1;35mVerifying the blockchain after modification:\033[0m")
-	fmt.Println("\033[1;35m---------------------------------------------\033[0m")
+	// Verify the blockchain integrity
+	fmt.Println("\n-****************************************************************************************************\n")
+	fmt.Println("                        Verifying Blockchain integrity after Modification")
+	fmt.Println("\n****************************************************************************************************\n")
 	assignment01bca.VerifyChain()
 }
